b12da231f346b03cf54885399b2e7de170deae84
Sarthak Vaishampayan <149432829+SarthakVaishampayan@users.noreply.github.com>
2025-04-16 12:46:05 +0530

    minor update 1

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,12 +17,25 @@ const app = express();
 app.use(express.json());
 
 // Enable CORS
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://bookyourgame-git-main-sarthak-vaishampayans-projects.vercel.app',
+  'https://bookyourgame-n2ynujvdb-sarthak-vaishampayans-projects.vercel.app'
+];
+
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
+
 app.use(cors(corsOptions));
 
 // Dev logging middleware
